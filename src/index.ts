63e2eba9418d66ff6fc53443f351890a3038970f
s-sreedhar <s.sreedhargoud@gmail.com>
2025-08-06 22:26:12 +0530

    updated config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-// import type { Core } from '@strapi/strapi';
+// // import type { Core } from '@strapi/strapi';
 
-export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+// export default {
+//   /**
+//    * An asynchronous register function that runs before
+//    * your application is initialized.
+//    *
+//    * This gives you an opportunity to extend code.
+//    */
+//   register(/* { strapi }: { strapi: Core.Strapi } */) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+//   /**
+//    * An asynchronous bootstrap function that runs before
+//    * your application gets started.
+//    *
+//    * This gives you an opportunity to set up your data model,
+//    * run jobs, or perform some special logic.
+//    */
+//   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+// };
+
+module.exports = {
+  register({ strapi }) {
+    strapi.server.router.get('/_health', (ctx) => {
+      ctx.set('Content-Type', 'text/plain');
+      ctx.body = 'OK';
+    });
+  },
 };
